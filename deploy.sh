52609640eea491a360bb320b827234943c420dbf
LEDUNOIS Simon <simon.ledunois@dental-wings.com>
2023-05-24 11:53:56 -0400

    feat: add redis kubernetes
    Change-Id: I6befad158975e9a9985f5df1568dced05f736c2e

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+CLUSTERED_SUFIX=""
+
+if [[ $* == *-c* ]]
+then 
+  CLUSTERED_SUFIX="-cluster"
+fi
+
+kubectl apply -f namespace.yml
+kubectl apply -f storage-class.yml
+
+/bin/bash ./redis$CLUSTERED_SUFIX/deploy.sh
---
+++ redis/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+STEPS=4
+
+FOLDER=$( dirname -- "${BASH_SOURCE[0]}" )
+
+echo "[redis] [1/${STEPS}] Creating PersistentVolume"
+kubectl apply -f $FOLDER/persistent-volume.yml
+
+echo "[redis] [2/${STEPS}] Creating ConfigMap"
+kubectl apply -n bigblueswarm -f $FOLDER/config-map.yml
+
+echo "[redis] [3/${STEPS}] Creating Statefulset"
+kubectl apply -n bigblueswarm -f $FOLDER/statefulset.yml
+
+echo "[redis] [4/${STEPS}] Creating Service"
+kubectl apply -n bigblueswarm -f $FOLDER/service.yml
